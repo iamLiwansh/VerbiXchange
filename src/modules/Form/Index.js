860a982f023330b02eb868220ba1a6da058ba558
iamLiwansh <saini.liwansh@gmail.com>
2024-08-29 20:17:05 +0530

    added styling

---
+++ src/modules/Form/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Input from '../../components/Input';
+
+const Form = () => {
+  return (
+    <div className='bg-white w-[600px] h-[600px] shadow-lg'>
+        <h1>WelCome</h1>
+        <h2>Sign-up here</h2>
+        {/* <Input/> */}
+    </div>
+  )
+}
+
+export default Form
