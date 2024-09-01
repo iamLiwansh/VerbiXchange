54d607f8b0b700006bbc8f1fdf399aee1f7794d1
iamLiwansh <saini.liwansh@gmail.com>
2024-09-01 22:10:16 +0530

    completed left side

--- src/modules/Dashboard/Index.js
+++ src/modules/Dashboard/Index.js
@@ -1,24 +1,82 @@
-import React from 'react'
-import Avatar from '../../assets/avatar.svg'
+import React from "react";
+import Avatar from "../../assets/avatar.svg";
 
-const Dashboard
- = () => {
+const Dashboard = () => {
+  const contacts = [
+    {
+      name: "Kapil",
+      status: "Available",
+      img: Avatar,
+    },
+    {
+      name: "Rahul",
+      status: "Available",
+      img: Avatar,
+    },
+    {
+      name: "Varun",
+      status: "Available",
+      img: Avatar,
+    },
+    {
+      name: "Mukul",
+      status: "Available",
+      img: Avatar,
+    },
+    {
+      name: "Mayank",
+      status: "Available",
+      img: Avatar,
+    },
+    {
+      name: "Deepu",
+      status: "Available",
+      img: Avatar,
+    },
+  ];
+
   return (
-    <div className='w-screen flex'>
-        <div className='w-[25%] h-screen bg-secondary'>
-            <div className='flex justify-center items-center my-8'>
-            <img src={Avatar} alt='user' height={50} width={50}></img>
-            <div className='ml-4'>
-            <h1 className='text-2xl'>Liwansh Saini</h1>
+    <div className="w-screen flex">
+      <div className="w-[25%] h-screen bg-secondary">
+        <div className="flex  items-center my-8 mx-14">
+          <img
+            className="border border-primary p-[2px] rounded-full"
+            src={Avatar}
+            alt="user"
+            height={50}
+            width={50}
+          />
+          <div className="ml-4">
+            <h1 className="text-2xl">Liwansh Saini</h1>
             <h1 className="text-lg font-light">my account</h1>
-            </div>
-            </div>
-            <hr/>
+          </div>
         </div>
-        <div className='w-[50%] h-screen'></div>
-        <div className='w-[25%] h-screen'></div>
-    </div>
-  )
-}
+        <hr />
 
-export default Dashboard
+        <div className="mx-14 mt-10">
+          <div className="text-primary text-lg">Messages</div>
+          {contacts.map(({ name, status, img }, index) => {
+            return (
+              <div
+                className="flex items-center py-8 border-b border-b-gray-300"
+                key={index}
+              >
+                <div className="cursor-pointer flex items-center">
+                <img src={img} alt={name} height={40} width={40} />
+                <div className="ml-6">
+                  <h1 className="text-lg font-semibold">{name}</h1>
+                  <h1 className="text-lg font-light text-gray-600">{status}</h1>
+                </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="w-[50%] h-screen bg-white"></div>
+      <div className="w-[25%] h-screen"></div>
+    </div>
+  );
+};
+
+export default Dashboard;
