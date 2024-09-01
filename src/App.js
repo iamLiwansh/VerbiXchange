9df8477546edc5195e59b509497b7afae8de1062
iamLiwansh <saini.liwansh@gmail.com>
2024-09-01 17:33:58 +0530

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Dashboard from './modules/Dashboard/Index';
 import Form from './modules/Form/Index';
 function App() {
   return (
    <div className="bg-[#c8d7ee] h-screen flex justify-center items-center">
-    <Form/>
+    {/* <Form/> */}
+    <Dashboard/>
    </div>
   );
 }
