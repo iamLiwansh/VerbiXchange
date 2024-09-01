804f06e937a40e81db711223d29d6b662f9d3f0b
iamLiwansh <saini.liwansh@gmail.com>
2024-09-01 21:02:36 +0530

    added left side

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Dashboard from './modules/Dashboard/Index';
-import Form from './modules/Form/Index';
+// import Form from './modules/Form/Index';
 function App() {
   return (
    <div className="bg-[#c8d7ee] h-screen flex justify-center items-center">
--- src/modules/Dashboard/Index.js
+++ src/modules/Dashboard/Index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Avatar from '../../assets/avatar.svg'
 
 const Dashboard
  = () => {
   return (
-    <div>Dashboard
-        
+    <div className='w-screen flex'>
+        <div className='w-[25%] h-screen bg-secondary'>
+            <div className='flex justify-center items-center my-8'>
+            <img src={Avatar} alt='user' height={50} width={50}></img>
+            <div className='ml-4'>
+            <h1 className='text-2xl'>Liwansh Saini</h1>
+            <h1 className="text-lg font-light">my account</h1>
+            </div>
+            </div>
+            <hr/>
+        </div>
+        <div className='w-[50%] h-screen'></div>
+        <div className='w-[25%] h-screen'></div>
     </div>
   )
 }
