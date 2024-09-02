81285deda0a4dff8abc41bbda32c8357bb1b3908
iamLiwansh <saini.liwansh@gmail.com>
2024-09-02 22:48:35 +0530

    creating chat section with call

--- src/modules/Dashboard/Index.js
+++ src/modules/Dashboard/Index.js
@@ -62,11 +62,13 @@ const Dashboard = () => {
                 key={index}
               >
                 <div className="cursor-pointer flex items-center">
-                <img src={img} alt={name} height={40} width={40} />
-                <div className="ml-6">
-                  <h1 className="text-lg font-semibold">{name}</h1>
-                  <h1 className="text-lg font-light text-gray-600">{status}</h1>
-                </div>
+                  <img src={img} alt={name} height={40} width={40} />
+                  <div className="ml-6">
+                    <h1 className="text-lg font-semibold">{name}</h1>
+                    <h1 className="text-lg font-light text-gray-600">
+                      {status}
+                    </h1>
+                  </div>
                 </div>
               </div>
             );
@@ -74,10 +76,34 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="w-[50%] h-screen bg-white flex flex-col items-center">
-          <div className="w-[75%] bg-secondary h-[80px] mt-14 rounded-full flex items-center px-14">
-            <div> <img src={Avatar} alt="user" height={40} width={40}/></div>
-            <h3  className="text-lg ml-6">Kapil</h3>
+        <div className="w-[75%] bg-secondary h-[80px] mt-14 rounded-full flex items-center px-14">
+          <div>
+            <img src={Avatar} alt="user" height={40} width={40} />
           </div>
+          <div className="ml-6 mr-auto ">
+            <h3 className="text-lg ">Kapil</h3>
+            <h1 className="text-lg font-light text-gray-600">online</h1>
+          </div>
+          <div className="cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-phone-outgoing"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2c-8.072 -.49 -14.51 -6.928 -15 -15a2 2 0 0 1 2 -2" />
+              <path d="M15 5h6" />
+              <path d="M18.5 7.5l2.5 -2.5l-2.5 -2.5" />
+            </svg>
+          </div>
+        </div>
       </div>
       <div className="w-[25%] h-screen"></div>
     </div>
