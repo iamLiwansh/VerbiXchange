778af8f31dd019ad925201c36298f30b8e274fdb
iamLiwansh <saini.liwansh@gmail.com>
2024-09-07 22:46:04 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './modules/Dashboard/Index';
-// import Form from './modules/Form/Index';
+import Form from './modules/Form/Index';
 function App() {
   return (
       <>
     <Routes>
-      <Route path="/user/sign-in"/>
+    <Route path="/" element={<Dashboard/>}/>
+      <Route path="/user/sign-in" element={<Form isSignInPage={true}/>}/>
+      <Route path="/user/sign-up" element={<Form isSignInPage={false}/>}/>
     </Routes>
    <div className="bg-[#c8d7ee] h-screen flex justify-center items-center">
     {/* <Form/> */}
