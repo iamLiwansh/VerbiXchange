adeab161c41df7f0ec0478fa634440dcecca2ff1
iamLiwansh <saini.liwansh@gmail.com>
2024-09-07 22:41:04 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './modules/Dashboard/Index';
 // import Form from './modules/Form/Index';
 function App() {
   return (
+      <>
+    <Routes>
+      <Route path="/user/sign-in"/>
+    </Routes>
    <div className="bg-[#c8d7ee] h-screen flex justify-center items-center">
     {/* <Form/> */}
     <Dashboard/>
    </div>
+   </>
   );
 }
 
