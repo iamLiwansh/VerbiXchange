4f8327221d51027f8c63465dfcb46ca8e09bfc64
iamLiwansh <saini.liwansh@gmail.com>
2024-09-07 23:26:04 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Dashboard from './modules/Dashboard/Index';
-import Form from './modules/Form/Index';
+import { Route, Routes, Navigate} from "react-router-dom";
+import "./App.css";
+import Dashboard from "./modules/Dashboard/Index";
+import Form from "./modules/Form/Index";
 
-const ProtectedRoutes = ({ children}) => {
-  const isLoggedIn = localStorage.getItem('user:token') !== null
-}
+
+const ProtectedRoutes = ({ children }) => {
+  
+  const isLoggedIn = localStorage.getItem("user:token") !== null || true;
+  // console.log(isLoggedIn);
+
+  if (!isLoggedIn){
+    return <Navigate to={'/users/sign_in'}/>
+    
+  } 
+  return children;
+};
+
 function App() {
   return (
-      <>
-    <Routes>
-    <Route path="/" element={<Dashboard/>}/>
-      <Route path="/users/sign_in" element={<Form isSignInPage={true}/>}/>
-      <Route path="/users/sign_up" element={<Form isSignInPage={false}/>}/>
-    </Routes>
-   </>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <Dashboard />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="/users/sign_in" element={<Form isSignInPage={true} />} />
+        <Route path="/users/sign_up" element={<Form isSignInPage={false} />} />
+      </Routes>
+    </>
   );
 }
 
