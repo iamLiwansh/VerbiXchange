ce5318b0ab2f9155fae9a907f8c19701c14ca562
iamLiwansh <saini.liwansh@gmail.com>
2024-09-11 21:50:02 +0530

    deleted message

--- src/modules/Dashboard/Index.js
+++ src/modules/Dashboard/Index.js
@@ -134,7 +134,7 @@ const Dashboard = () => {
               lorem ispum is simplt dummy text fr peronmf amd irierf ffd
             </div>
             <div className="max-w-[40%] bg-primary rounded-b-xl rounded-tl-xl ml-auto p-4 text-white mb-6">
-              lorem ispum is simplt dummy text fr peronmf amd irierf ddfd
+              lorem ispum is simplt dummy text fr peronmf amd irierf dd
             </div>
             <div className="max-w-[40%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
               lorem ispum is simplt dummy text fr peronmf amd irierfd dfhhgfd
